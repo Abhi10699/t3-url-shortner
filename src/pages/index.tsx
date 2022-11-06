fba17ae43a012a564af252da1b5cda44739be061
Abhi Patel <abhi@codise.us>
2022-11-07 00:23:16 +0530

    Initial Commit �

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>URL Shortner</title>
+        <meta name="description" content="A Modern URL Shortner" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+      
+      </main>
+    </>
+  );
+};
+
+export default Home;
